78673e70dd8f9c0e15162b66d9e7f436fe2fd105
Kishan Sah <kishansah7086@gmail.com>
2025-02-24 01:28:52 +0530

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', // Allow all origins (for testing)
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Allow cookies & auth headers
-    allowedHeaders: 'Content-Type, Authorization',
+    origin: '*', // Or set to your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
